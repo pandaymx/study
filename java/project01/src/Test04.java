2393676a98f4005349fbfebaa14aa61ef0e1da98
皮皮萌宝 <ppmblszdp@189.com>
2023-06-02 05:41:41 +0800

    输入输出

---
+++ java/project01/src/Test04.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test04 {
+    public static void main(String[] args) {
+        // 输入
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        System.out.println(next);
+    }
+}
