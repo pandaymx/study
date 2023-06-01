e1d81735c2cb6a323f88733540aad5117efd3328
皮皮萌宝 <ppmblszdp@189.com>
2023-06-02 05:33:29 +0800

    字面量

---
+++ java/project01/src/Test02.java
@@ -0,0 +1,14 @@
+public class Test02 {
+    public static void main(String[] args) {
+        // 整数字面量
+        System.out.println(6);
+        // 小数字面量
+        System.out.println(6.0);
+        // 字符串字面量
+        System.out.println("6");
+        // 字符字面量
+        System.out.println('6');
+        // 布尔型字面量
+        System.out.println(true);
+    }
+}
