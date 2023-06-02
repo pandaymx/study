cbd219d8474486e4d9a33afc52b238329e7547f0
皮皮萌宝 <ppmblszdp@189.com>
2023-06-02 18:29:02 +0800

    实体类

--- mybatis/mybatis-01/src/main/java/com/ppmb/pojo/User.java
+++ mybatis/mybatis-01/src/main/java/com/ppmb/pojo/User.java
@@ -1,4 +1,15 @@
 package com.ppmb.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
 }
